afdbdf4e1986110a63ba6705425cae3cfb07c3bd
root <root@bastion.0047.example.opentlc.com>
2020-02-27 21:48:40 +0000

    27022020

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+export TOWER_GUID=0047
+export OSP_GUID=c2c8
+export OPENTLC_LOGIN=julio.barrios-atos.net
+export OPENTLC_PASSWORD=B#p_a3m$v@i
+export GITHUB_REPO=https://github.com/jrramosb/ansible_advance_homework
+export JQ_REPO_BASE=http://www.opentlc.com/download/ansible_bootcamp
+export REGION=us-east-1
+export RH_MAIL_ID=julio.barrios
