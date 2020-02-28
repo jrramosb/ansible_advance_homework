7b683b96b1e770dfd4b5e47447f1269f7896958c
root <root@bastion.0047.example.opentlc.com>
2020-02-28 00:11:37 +0000

    Password change

--- variables.sh
+++ variables.sh
@@ -2,7 +2,7 @@
 export TOWER_GUID=0047
 export OSP_GUID=c2c8
 export OPENTLC_LOGIN=julio.barrios-atos.net
-export OPENTLC_PASSWORD=B#p_a3m$v@i
+export OPENTLC_PASSWORD=Ansible123
 export GITHUB_REPO=https://github.com/jrramosb/ansible_advance_homework
 export JQ_REPO_BASE=http://www.opentlc.com/download/ansible_bootcamp
 export REGION=us-east-1
